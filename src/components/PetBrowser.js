a0522aa5dcb583c26ac30341859b89752a9c0648
Oliver Spring <oliver9589@gmail.com>
2017-09-19 06:36:09 +0000

    automatically backed up by learn

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -5,7 +5,6 @@ class PetBrowser extends React.Component {
   render() {
     const pets = this.props.pets.map((pet, index) => (
         <Pet pet={pet}
-             key={index}
              onAdoptPet={this.props.onAdoptPet}
              isAdopted={this.props.adoptedPets.includes(pet.id)}/>
     ))
