093c50921fedb7046952f3f94975c63792882a7e
Oliver Spring <oliver9589@gmail.com>
2017-09-19 05:29:48 +0000

    automatically backed up by learn

--- src/components/App.js
+++ src/components/App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
     switch(this.state.filters.type) {
       case 'all':
         this.setState({
-          ...this.state
+          ...this.state,
           pets: fetch('/api/pets')
         })
         break;
