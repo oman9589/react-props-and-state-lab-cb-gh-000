32edc0b0b09f117105ffbf939bb6834576f6dbd2
Oliver Spring <oliver9589@gmail.com>
2017-09-19 04:25:26 +0000

    automatically backed up by learn

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -9,13 +9,13 @@ class Pet extends React.Component {
     return (
       <div className="card">
         <div className="content">
-          <a className="header">this.props.name {this.props.gender === 'male' ? '♂' : '♀'}</a>
+          <a className="header">{this.props.name} {this.props.gender === 'male' ? '♂' : '♀'}</a>
           <div className="meta">
-            <span className="date">this.props.type</span>
+            <span className="date">{this.props.type}</span>
           </div>
           <div className="description">
-            <p>Age: this.props.age</p>
-            <p>Weight: this.props.weight</p>
+            <p>Age: {this.props.age}</p>
+            <p>Weight: {this.props.weight}</p>
           </div>
         </div>
         <div className="extra content">
