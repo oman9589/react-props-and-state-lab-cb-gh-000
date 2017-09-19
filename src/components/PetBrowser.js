c973cbcb2027dcebbd51f0d61e3e88b461fcdd47
Oliver Spring <oliver9589@gmail.com>
2017-09-19 04:36:53 +0000

    automatically backed up by learn

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -6,7 +6,7 @@ class PetBrowser extends React.Component {
   render() {
     const pets = () => {
       this.props.pets.map((pet) => {
-        return <Pet name={pet.name} age={pet.age} gender={pet.gender} type={pet.type}/>
+        return <Pet />
       })
     }
     return (
