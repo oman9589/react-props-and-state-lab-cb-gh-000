7cb5891d3e6d5d67272edcc39e56af1c2a6afa29
Oliver Spring <oliver9589@gmail.com>
2017-09-19 05:43:38 +0000

    automatically backed up by learn

--- src/components/App.js
+++ src/components/App.js
@@ -69,7 +69,7 @@ class App extends React.Component {
             </div>
             <div className="twelve wide column">
               <PetBrowser pets={this.state.pets}
-                          onAdoptPet={() => return null;}
+                          onAdoptPet={this.handleAdoptPet}
                           adoptedPets={this.state.adoptedPets}/>
             </div>
           </div>
