63f3e929e55e5bf2d20b2910de833598a8d4af2e
Oliver Spring <oliver9589@gmail.com>
2017-09-19 05:37:43 +0000

    automatically backed up by learn

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -11,7 +11,7 @@ class PetBrowser extends React.Component {
                type={pet.type}
                onAdoptPet={this.props.onAdoptPet}
                isAdopted={this.props.adoptedPets.contains(this.pet.name)}/>
-      ))}
+      ))
     }
     return (
       <div className="ui cards">
