34ae136eb56dfbfa869f60b7ef0068bfb35a1999
Oliver Spring <oliver9589@gmail.com>
2017-09-19 05:11:24 +0000

    automatically backed up by learn

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -10,7 +10,7 @@ class Filters extends React.Component {
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" onChange={this.handleChange} value={this.props.filters}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
