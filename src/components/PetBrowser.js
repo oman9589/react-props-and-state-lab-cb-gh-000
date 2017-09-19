4c53b9cdf0041559b4f83f442ffdad39fc72d3c1
Oliver Spring <oliver9589@gmail.com>
2017-09-19 05:51:33 +0000

    automatically backed up by learn

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -5,7 +5,7 @@ class PetBrowser extends React.Component {
   render() {
     return (
       <div className="ui cards">
-      {window.alert('test')}
+      {window.alert(this.props.pet)}
         {this.props.pets.map((pet, index) => (
             <Pet name={pet.name}
                  gender={pet.gender}
