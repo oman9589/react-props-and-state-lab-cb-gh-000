b77b29cd676c86acb24e57210bc6344f3a92e0e2
Oliver Spring <oliver9589@gmail.com>
2017-09-19 05:26:11 +0000

    automatically backed up by learn

--- src/components/App.js
+++ src/components/App.js
@@ -25,6 +25,19 @@ class App extends React.Component {
     })
   }
 
+  handleFindPetsClick = () => {
+    switch(this.state.filters.type) {
+      case 'all':
+        break;
+      case 'cat':
+        break;
+      case 'dog':
+        break;
+      case 'micropig':
+        break;
+    }
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -34,7 +47,9 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters filters={this.state.filters.type} onChangeType={this.handleChangeType}/>
+              <Filters filters={this.state.filters.type}
+                       onChangeType={this.handleChangeType}
+                       onFindPetsClick{this.handleFindPetsClick}/>
             </div>
             <div className="twelve wide column">
               <PetBrowser pets={this.state.pets}/>
