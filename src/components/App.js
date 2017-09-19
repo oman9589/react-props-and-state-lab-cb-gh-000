27f6788b3625a7a83efce70fa808d01ed5671449
Oliver Spring <oliver9589@gmail.com>
2017-09-19 06:09:28 +0000

    automatically backed up by learn

--- src/components/App.js
+++ src/components/App.js
@@ -30,7 +30,7 @@ class App extends React.Component {
       case 'all':
         this.setState({
           ...this.state,
-          pets: {this.fetch('/api/pets')}
+          pets: {fetch('/api/pets')}
         })
         window.alert('test');
         break;
